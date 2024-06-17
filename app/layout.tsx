5fa57ed5e8465966a47743ea5a09b9b74a83d0d5
villi02 <villiarnar1@gmail.com>
2024-06-17 22:52:46 +0200

    added navbar to layut

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +14,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main className="container mx-auto p-4">{children}</main>
+      </body>
     </html>
   );
 }
