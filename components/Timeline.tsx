9e7d36761d727b75d6db04f21d5346dac64378fb
villi02 <villiarnar1@gmail.com>
2024-06-19 22:10:58 +0200

    revamped timeline

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -19,7 +19,6 @@ const milestones: Milestone[] = [
     date: '2021-06-01',
     title: 'Internship at Tech Company',
     description: 'Worked on developing machine learning algorithms.',
-    image: 'https://via.placeholder.com/150', // Example image URL
   },
   {
     date: '2022-09-01',
@@ -46,7 +45,7 @@ const Timeline: React.FC = () => {
               <>
                 <div className="w-1/2 pr-8 flex justify-end">
                   <div className="text-right">
-                    <h3 className="text-lg font-semibold text-gray-900">{milestone.title}</h3>
+                    <h3 className="text-lg font-bold text-white">{milestone.title}</h3>
                     <time className="block text-sm font-normal text-gray-400">{milestone.date}</time>
                     <p className="text-base font-normal text-gray-500">{milestone.description}</p>
                     {milestone.image && (
@@ -56,46 +55,14 @@ const Timeline: React.FC = () => {
                     )}
                   </div>
                 </div>
-                <div className="relative w-1/2">
-                  <div className="w-6 h-6 bg-blue-500 rounded-full ring-8 ring-white flex items-center justify-center absolute left-1/2 transform -translate-x-1/2">
-                    <svg
-                      aria-hidden="true"
-                      className="w-3 h-3 text-white"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11V7a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </div>
-                </div>
+                <div className="w-1/2"></div>
               </>
             ) : (
               <>
-                <div className="relative w-1/2">
-                  <div className="w-6 h-6 bg-blue-500 rounded-full ring-8 ring-white flex items-center justify-center absolute right-1/2 transform translate-x-1/2">
-                    <svg
-                      aria-hidden="true"
-                      className="w-3 h-3 text-white"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11V7a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </div>
-                </div>
+                <div className="w-1/2"></div>
                 <div className="w-1/2 pl-8">
                   <div className="text-left">
-                    <h3 className="text-lg font-semibold text-gray-900">{milestone.title}</h3>
+                    <h3 className="text-lg font-bold text-white">{milestone.title}</h3>
                     <time className="block text-sm font-normal text-gray-400">{milestone.date}</time>
                     <p className="text-base font-normal text-gray-500">{milestone.description}</p>
                     {milestone.image && (
