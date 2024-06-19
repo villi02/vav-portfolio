50e7ebac7c810d030e7ce0613cf565bd1006360f
villi02 <villiarnar1@gmail.com>
2024-06-19 22:58:55 +0200

    created resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import PdfViewer from "@/components/PdfViewer";
+import React from 'react';
+import PdfViewer from '@/components/PdfViewer';
 
 const ResumePage: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">My Resume</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center">My Resume</h1>
       <PdfViewer />
     </div>
   );
