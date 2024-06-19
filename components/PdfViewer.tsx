310c35582c72db6073cf353131de43041bb58156
villi02 <villiarnar1@gmail.com>
2024-06-19 22:44:59 +0200

    revamped pdf viewer

--- components/PdfViewer.tsx
+++ components/PdfViewer.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Viewer, Worker } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import React from "react";
 
 const PdfViewer: React.FC = () => {
   return (
     <div className="pdf-container">
-      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}>
-        <Viewer fileUrl="/resume.pdf" />
-      </Worker>
+      <iframe
+        src="/resume.pdf"
+        width="100%"
+        height="100%"
+        style={{ border: "none" }}
+      />
     </div>
   );
 };
