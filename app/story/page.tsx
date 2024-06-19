378337d3535c3f357db60e9d0aba315302909a7e
villi02 <villiarnar1@gmail.com>
2024-06-19 22:31:09 +0200

    added story page

---
+++ app/story/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import Timeline from "@/components/Timeline"; // Adjust the path as needed
+
+const Page: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <div>
+        <div className="info-section p-6">
+          <h1>Welcome to My Website</h1>
+          <p>This is a brief introduction.</p>
+        </div>
+        <Timeline />
+      </div>
+    </React.StrictMode>
+  );
+};
+
+export default Page;
