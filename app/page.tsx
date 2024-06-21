dd8138972305002a9a9107051ed2af30c937c46a
villi02 <villiarnar1@gmail.com>
2024-06-21 23:35:14 +0200

    updated front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
-}
+"use client";
+import React from "react";
+import Timeline from "@/components/Timeline"; // Adjust the path as needed
+
+const Page: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <div>
+        <div className="info-section p-6">
+          <h1>Welcome to My Website</h1>
+          <p>This is a brief introduction.</p>
+        </div>
+      </div>
+    </React.StrictMode>
+  );
+};
+
+export default Page;
