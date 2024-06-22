a1f27cb3554e546586c2ae0d3ce1861816d80ae0
villi02 <villiarnar1@gmail.com>
2024-06-22 22:01:37 +0200

    added rough milestones

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -10,64 +10,73 @@ interface Milestone {
 
 const milestones: Milestone[] = [
   {
-    date: "2020-01-01",
+    date: "2021-08-01",
     title: "Started University",
-    description: "Began my journey in Computer Science and Mathematics.",
+    description:
+      "Began my M.Sc degrees in Computer Science and Mathematics at NTNU.",
     image: "https://via.placeholder.com/150",
   },
   {
-    date: "2021-06-01",
-    title: "Internship at Tech Company",
-    description: "Worked on developing machine learning algorithms.",
+    date: "2021-08-01",
+    title: "Key Account Manager at Propulse NTNU",
+    description:
+      "Secured funding and partnerships for the student rocketry team.",
   },
   {
-    date: "2022-09-01",
-    title: "Research Assistant",
-    description: "Conducted research in AI and contributed to academic papers.",
+    date: "2022-02-01",
+    title: "Project Lead at Cogito NTNU",
+    description:
+      "Led AI projects, including pneumonia detection and time series prediction.",
+  },
+  {
+    date: "2022-02-01",
+    title: "Software Engineer at Favn Software",
+    description:
+      "Implemented scalable data pipeline and enhanced security for applications.",
+  },
+  {
+    date: "2022-02-01",
+    title: "3rd Place at NASA JPL Hackathon",
+    description:
+      "Achieved 3rd place in a hackathon hosted by Start NTNU and NASA JPL.",
+  },
+  {
+    date: "2023-07-01",
+    title: "AI Research Assistant at NorwAI",
+    description:
+      "Contributed to AI research, paper writing, and optimized dataset processing.",
     image: "https://via.placeholder.com/150",
   },
   {
-    date: "2023-06-01",
+    date: "2023-09-01",
+    title: "Finalist at HackMIT 2023",
+    description: "Reached the finals in HackMIT 2023 competition.",
+  },
+  {
+    date: "2023-10-01",
+    title: "Chief Investment Officer at Høiskolens Chemikerforening",
+    description: "Founded and managed an endowment, handling investments.",
+  },
+  {
+    date: "2024-02-01",
+    title: "2nd Place in ABB Group Hackathon",
+    description:
+      "Secured 2nd place in a hackathon organized by ABB Group and NTNU IE-faculty.",
+  },
+  {
+    date: "2024-03-01",
+    title: "Co-founder at A* Consulting",
+    description:
+      "Featured in Adresseavisa, secured first three customers within a month.",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    date: "2024-07-01",
     title: "Graduation",
     description:
-      "Completed my M.Sc degrees in Computer Science and Mathematics.",
-  },
-  {
-    date: "2021-06-01",
-    title: "Internship at Tech Company",
-    description: "Worked on developing machine learning algorithms.",
-  },
-  {
-    date: "2022-09-01",
-    title: "Research Assistant",
-    description: "Conducted research in AI and contributed to academic papers.",
-    image: "https://via.placeholder.com/150",
-  },
-  {
-    date: "2023-06-01",
-    title: "Graduation",
-    description:
-      "Completed my M.Sc degrees in Computer Science and Mathematics.",
-  },
-  {
-    date: "2021-06-01",
-    title: "Internship at Tech Company",
-    description: "Worked on developing machine learning algorithms.",
-  },
-  {
-    date: "2022-09-01",
-    title: "Research Assistant",
-    description: "Conducted research in AI and contributed to academic papers.",
-    image: "https://via.placeholder.com/150",
-  },
-  {
-    date: "2023-06-01",
-    title: "Graduation",
-    description:
-      "Completed my M.Sc degrees in Computer Science and Mathematics.",
+      "Expected completion of M.Sc degrees in Computer Science and Mathematics.",
   },
 ];
-
 const Timeline: React.FC = () => {
   const elementsRef = useRef<(HTMLDivElement | null)[]>([]);
 
@@ -106,7 +115,10 @@ const Timeline: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-8">My Timeline</h1>
+      <h1 className="text-3xl font-bold mb-8">
+        My <strong className="text-purple-500">Timeline</strong>
+      </h1>
+      <p className="mb-8">My personal milestones and highlights</p>
       <div className="relative w-full max-w-4xl mx-auto">
         <div className="absolute top-0 bottom-0 left-1/2 w-0.5 bg-gray-200 transform -translate-x-1/2"></div>
         {milestones.map((milestone, index) => (
