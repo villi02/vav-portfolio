60822368c7a7075667531138278db0366b74cb9f
villi02 <villiarnar1@gmail.com>
2024-06-28 23:59:56 +0200

    bigger icons and fixed allignment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,28 +7,27 @@ const Footer: React.FC = () => {
     <footer className="footer bg-transparent-800 pt-4 pb-2 border-t border-white">
       <Container>
         <Row className="justify-content-between align-items-center">
-          <Col xs={12} sm={6}>
-            <h1 style={{ color: "white" }}>VAV</h1>
+          <Col xs={12} sm={6} className="d-flex align-items-center" style={{ paddingRight: '15px' }}>
+            <h1 style={{ color: "white", fontSize: '36px', paddingLeft: '20px', paddingRight: '15px' }}>VAV</h1>
           </Col>
-          <Col xs={12} sm={6} className="text-end">
+          <Col xs={12} sm={6} className="text-end" style={{ paddingRight: '15px' }}>
             <div className="d-flex flex-column align-items-end">
               <div className="d-flex mb-2">
                 <a
                   href="https://www.linkedin.com/in/vilhjalmur-arnar-vilhjalmsson/"
-                  className="group relative inline-flex items-center justify-center h-10 w-10 rounded-full border border-white text-white bg-black hover:bg-white hover:text-black transition-all duration-300 me-2"
+                  className="group relative inline-flex items-center justify-center h-12 w-12 rounded-full border border-white text-white bg-black hover:bg-white hover:text-black transition-all duration-300 me-2"  // Increased size to h-12 w-12
                 >
-                  <FaLinkedin className="h-6 w-6" aria-hidden="true" />
+                  <FaLinkedin className="h-8 w-8" aria-hidden="true" />
                 </a>
                 <a
                   href="https://github.com/villi02"
-                  className="group relative inline-flex items-center justify-center h-10 w-10 rounded-full border border-white text-white bg-black hover:bg-white hover:text-black transition-all duration-300"
+                  className="group relative inline-flex items-center justify-center h-12 w-12 rounded-full border border-white text-white bg-black hover:bg-white hover:text-black transition-all duration-300"
                 >
-                  <FaGithub className="h-6 w-6" aria-hidden="true" />
+                  <FaGithub className="h-8 w-8" aria-hidden="true" /> 
                 </a>
               </div>
               <p className="text-gray-300 text-xs">
-                Copyright 2023. All Rights Reserved Vilhjalmur Arnar
-                Vilhjalmsson
+                Copyright 2023. All Rights Reserved Vilhjalmur Arnar Vilhjalmsson
               </p>
             </div>
           </Col>
