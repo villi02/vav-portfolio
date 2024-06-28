1695bc8504006668346e4308030f5c25aceaacd6
villi02 <villiarnar1@gmail.com>
2024-06-28 23:07:27 +0200

    added landing page banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 import React from "react";
-import Timeline from "@/components/Timeline"; // Adjust the path as needed
+import LandingPage from "@/components/Banner";
 
 const Page: React.FC = () => {
   return (
     <React.StrictMode>
       <div>
-        <div className="info-section p-6">
-          <h1>Welcome to My Website</h1>
-          <p>This is a brief introduction.</p>
-        </div>
+        <LandingPage />
       </div>
     </React.StrictMode>
   );
