e8adc67e28135b7806b6f4ba4291014f96fa6ede
villi02 <villiarnar1@gmail.com>
2024-06-30 22:38:46 +0200

    Added hover coloring and fade

--- components/CaruselCard.tsx
+++ components/CaruselCard.tsx
@@ -1,3 +1,4 @@
+// CaruselCard.tsx
 import React from "react";
 
 interface ProjectCardProps {
@@ -13,11 +14,19 @@ export const CaruselCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className="w-full sm:w-1/2 md:w-1/3 p-4">
-      <div className="proj-imgbx shadow-lg rounded-lg overflow-hidden">
-        <img src={imgUrl} alt={title} className="w-full" />
-        <div className="proj-txtx p-4 bg-white">
-          <h4 className="text-xl font-bold">{title}</h4>
-          <span className="text-gray-600">{description}</span>
+      <div className="relative overflow-hidden shadow-lg rounded-lg mb-6 group">
+        <img
+          src={imgUrl}
+          alt={title}
+          className="w-full transition-transform duration-500 ease-in-out transform group-hover:scale-110"
+        />
+        {/* Adjust opacity to 0 by default and 100 on hover */}
+        <div className="absolute inset-0 bg-gradient-to-r from-purple-500 to-pink-500 transition-opacity duration-500 ease-in-out opacity-0 group-hover:opacity-100"></div>
+        <div className="absolute inset-0 flex items-center justify-center p-4 transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100">
+          <div className="text-center text-white">
+            <h4 className="text-2xl font-bold">{title}</h4>
+            <span className="italic font-normal text-sm">{description}</span>
+          </div>
         </div>
       </div>
     </div>
