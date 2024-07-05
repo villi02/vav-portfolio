32de28efaabe6696ba81c9744158916bb0eebd1e
villi02 <villiarnar1@gmail.com>
2024-07-05 23:17:10 +0200

    Update landing page with SearchSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import React from "react";
 import Banner from "@/components/Banner";
+import ProjectCarusel from "@/components/ProjectCarusel";
+import { SearchSection } from "@/components/SearchSection";
 
 const Page: React.FC = () => {
   return (
     <React.StrictMode>
       <div>
         <Banner />
+        <SearchSection />
+        <ProjectCarusel />
       </div>
     </React.StrictMode>
   );
