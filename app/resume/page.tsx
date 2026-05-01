9674b371138e230c95e324ca9f6b9175bda50ce6
villi02 <villiarnar1@gmail.com>
2026-05-01 23:17:52 +0800

    update: use ResponsiveViewer

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,47 +1,17 @@
 "use client";
 import React, { useState } from "react";
-import PdfViewer from "@/components/PdfViewer";
+import ResponsiveViewer from "@/components/ResponsiveViewer";
 
 const Page: React.FC = () => {
-  const resumeLinks: { [key: string]: string } = {
-    nonTechnical: "/VilhjalmurArnarVilhjalmsson-CV-Feb25.pdf",
-    technical: "/VAV-T-jul24.pdf",
-  };
-
-  const [activeResume, setActiveResume] = useState("nonTechnical");
-
-  const getButtonClass = (name: string) => {
-    return `py-2 px-4 rounded-full transition-all duration-300 ease-in-out ${
-      activeResume === name
-        ? "bg-gradient-to-r from-purple-500 to-pink-500 text-white"
-        : "bg-transparent text-white hover:bg-white hover:text-black border border-white"
-    }`;
-  };
+  const resumeLinkPdf = "VilhjalmurArnarVilhjalmsson-CV-Feb25.pdf";
+  const resumeLinkImg = "VilhjalmurArnarVilhjalmsson-CV-Feb25.png";
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">
         My <strong className="text-purple-500">Resume</strong>
       </h1>
-      <div className="flex justify-center space-x-4 mb-4">
-        <button
-          onClick={() => {
-            setActiveResume("nonTechnical");
-          }}
-          className={getButtonClass("nonTechnical")}
-        >
-          Non-Technical
-        </button>
-        <button
-          onClick={() => {
-            setActiveResume("technical");
-          }}
-          className={getButtonClass("technical")}
-        >
-          Technical
-        </button>
-      </div>
-      <PdfViewer resumeLink={resumeLinks[activeResume]} />
+      <ResponsiveViewer imgLink={resumeLinkImg} pdfLink={resumeLinkPdf} />
     </div>
   );
 };
